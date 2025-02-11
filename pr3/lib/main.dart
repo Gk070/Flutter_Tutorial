ebaed13b2e3c7970cad5091dcfb84970555d4df1
Gk070 <geethulkrishna@gmail.com>
2025-02-11 23:16:59 +0530

    .

---
+++ pr3/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text(
+        'Hello Ninjas',
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+    body: Center(
+      child: Text(
+          'Hello Ninjas',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+        ),
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text(
+        'Click'
+      ),
+      backgroundColor: Colors.red[600],
+    ),
+  ),
+));
